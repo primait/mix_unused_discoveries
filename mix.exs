c2304b4cf1203f864cd7dd1acb2a3f126ffb0f12
Mauro Rocchi <mauro.rocchi@prima.it>
2022-11-11 17:17:01 +0100

    update mix_unused

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule MixUnusedDiscoveries.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :pls, :test], runtime: false},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
-      {:mix_unused, github: "primait/mix_unused", branch: "unreachable-analyzer"},
+      {:mix_unused, github: "primait/mix_unused"},
       {:mock, "~> 0.3.7", only: :test}
     ]
   end
