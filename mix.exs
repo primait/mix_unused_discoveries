6054bbdce210ca6f9a187821bbbb6aadc6d22905
Alberto Fanton <alberto.fanton@prima.it>
2022-11-11 10:38:57 +0100

    Add dialyxir on test and pls envs

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule MixUnusedDiscoveries.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :pls, :test], runtime: false},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:mix_unused, github: "primait/mix_unused", branch: "unreachable-analyzer"},
       {:mock, "~> 0.3.7", only: :test}
